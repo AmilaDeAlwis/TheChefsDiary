963b7bf49de02c20326aa304d67b7e43565c24ad
AmilaDeAlwis <amilaalwis4@gmail.com>
2023-04-19 00:01:53 +0530

    change name using useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -8,14 +8,22 @@ const Home = () => {
         {name:'book three', author:'Mario', id:3},
     ])
 
+    const [name, setName] = useState('Mario');
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
 
+    useEffect(() =>{
+        console.log('UseEffect ran');
+        console.log(name);
+    }, [name]);
+
     return (  
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            <button onClick={() => setName('Luigi')}>Change Name</button>
         </div>
     );
 }
